133a9c087894ad7a5d4169f82632c7324d42eb50
Saqib Aziz <saqib.aziz41@gmail.com>
2022-07-22 02:13:53 -0700

    added filter in portfolio and new data

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,10 +4,27 @@ import Card from '../components/Card';
 import data from '../data/data.json'
 
 function Portfolio() {
-  const [projects, setprojects] = React.useState(data.projects)
-  console.log(data.projects[0].src);
+  const [projects, setProjects] = React.useState(data.projects)
+  const [choice, setChoice] = React.useState('all')
+
+  React.useEffect(() => {
+    if(choice === 'all'){
+      return setProjects(data.projects)
+    }
+    setProjects(data.projects.filter(project => project.type === choice))
+  }, [choice])
+
+  
   return <div className='container my-8 mx-auto'>
     <Breadcrum current='Portfolio' />
+
+    <div className="text-center">
+      <select onChange={(e) => setChoice(e.target.value)} class="select w-full max-w-xs text-center shadow-sm text-secondary">
+        <option value='all'>All</option>
+        <option value="backend">Backend</option>
+        <option value="frontend">Frontend</option>
+      </select>
+    </div>
     
     <div className="my-8">
       {projects.map((project, i) => (
